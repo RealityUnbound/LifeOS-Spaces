6626c61d30e0513e3e80e2a70c9f6251e8bac08e
RealityUnbound <149747643+RealityUnbound@users.noreply.github.com>
2024-05-28 18:14:25 -0400

    Not functional: Commit because I just had a powerout

---
+++ Assets/Scripts/Managers/RoomManager.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Grid), typeof(BoxCollider))]
+public class RoomManager : MonoBehaviour
+{
+    /// <summary>
+    /// Sets up room to be able to spawn objects to interact with
+    /// Meta and GorillaZila influenced system for managing rooms
+    /// </summary>
+
+    [Header("Dependencies")]
+    [SerializeField] OVRSceneManager sceneManager;
+
+    [Header("Settings")]
+    [SerializeField] LayerMask wallLayer;
+    [SerializeField] LayerMask furnitureLayer;
+    [SerializeField] float paddingFromFurniture = .2f;
+
+    private OVRSceneRoom room;
+    private BoxCollider roomBounds;
+    private Grid grid;
+    private TransformFollower follower;
+
+    private void Awake()
+    {
+        if (sceneManager == null)
+            sceneManager = GameObject.FindObjectOfType<OVRSceneManager>();
+
+        sceneManager.SceneModelLoadedSuccessfully += OnSceneLoaded;
+        grid = GetComponent<Grid>();
+        follower = GetComponent<TransformFollower>();
+        roomBounds = GetComponent<BoxCollider>();
+        roomBounds.isTrigger = true;
+    }
+    private void OnSceneLoaded()
+    {
+        room = GameObject.FindAnyObjectByType<OVRSceneRoom>();
+        room.gameObject.SetLayerRecursive("Room");
+
+        //Setup collider for room to fit the bounds of the space (using collider as a Bounds type doesn't work with rotation)
+        float height = room.Walls[0].Height;
+        float width = room.Floor.Width;
+        float depth = room.Floor.Height;
+        roomBounds.size = new Vector3(width, depth, height);
+        roomBounds.center = new Vector3(0, 0, height / 2f);
+        follower.target = room.Floor.transform;
+    }
+    public bool IsInsideGuardian(Vector3 position)
+    {
+        position.y = 0;
+        var result = OVRManager.boundary.TestPoint(position, OVRBoundary.BoundaryType.PlayArea);
+        float dist = result.ClosestDistance;
+        return dist <= .1f;
+    }
+
+    //Checks if the point is colliding with furniture using a Physics check box
+    public bool IsInsideFurniture(Vector3 position)
+    {
+        bool isInFurniture = Physics.CheckBox(position, grid.cellSize, transform.rotation, furnitureLayer);
+        bool isBelowFurniture = Physics.Raycast(position, Vector3.up, Mathf.Infinity, furnitureLayer);
+        return isInFurniture || isBelowFurniture;
+    }
+    //Returns true if a point is facing in the forward direction (i.e. the normal) of the closest wall to that given point
+    public bool IsPointInRoom(Vector3 point, float padding = 0)
+    {
+        float distance = DistanceToClosestWall(point, out Vector3 closestPoint, out OVRScenePlane closestWall);
+        if (distance < padding || closestWall == null)
+        {
+            return false;
+        }
+        bool isInBoxCollider = roomBounds.ClosestPoint(point) == point;
+        Vector3 dirToWall = (closestPoint - point).normalized;
+        float dotProduct = Vector3.Dot(closestWall.transform.forward, dirToWall);
+        return isInBoxCollider && dotProduct < -.5f;
+    }
+}
